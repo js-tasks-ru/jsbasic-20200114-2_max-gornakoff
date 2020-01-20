f807654950450d1f282bb168f7c812e5bfe91c39
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-20 21:40:43 +0300

    создал переменную result = 1, для первого произведения в цикле, задал цикл for, в условиях которого, создал переменную i счетчика поместив туда значение числа, котрое указывается в параметре функции (n), для вычисления факториала этого числа, задал условие окончания цикла, чтобы он выполнялся до тех пор пока i > 1, шаг цикла у меня уменьшение i на единицу после каждой итерации. В теле цикла задал произведение результата предыдущей итерации на результат предыдущей операции минус один. Вернул значение result на момент выхода из цикла.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,11 @@
  * @param {number} n
  * @returns {number}
  */
+'use strict';
 function factorial(n) {
-
-}
+	let result = 1;
+		for(let i=n; i > 1; i--){
+			result*= i;
+		}
+	return result;
+	}
