1bdc141db8281580764884af11b8491e02c63f72
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-21 21:51:04 +0300

    в теле функции задал условие, которое возвращает строку в исходном виде если параметр функции пустой, а если параметр не пустой то выполняется возведение первого символа в верхний регистр и приклеивание к нему остальной части строки. Затем выводится строка с первой заглавной буквой заданная в параметре функции

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,12 @@
  * @param {string} str
  * @returns {string}
  */
+'use strict';
 function ucFirst(str) {
+	if (str === ''){
+		return str;
+	} else {
+		return str[0].toUpperCase() + str.substring(1);
+	}
+	
 }
