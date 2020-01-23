274405dc5ec7c3798717bc45b5f50b2ba0a866db
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-23 23:52:36 +0300

    не понимаю, что я делаю не правильно

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
+'use strict';
 let calculator = {
-  // ваш код
+	read(a,b){
+	  this.a = +prompt('Введите A :');
+	  this.b = +prompt('Введите B :');
+  	},
+	sum(){
+		return this.a + this.b;
+	},
+	mul(){
+		return this.a * this.b;
+	},	
+	
 };
+
+calculator.read();
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
