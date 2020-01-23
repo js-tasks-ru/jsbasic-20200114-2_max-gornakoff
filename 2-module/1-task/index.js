c5bb506375dd3cc9869e3c3d09926a7014fd1891
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-23 09:57:55 +0300

    в теле функции создал переменную, которая будет хранить сумму зарплат. Так же создал условие, которое проверяет тип данных значения свойства объекта и, если это число то складывает значение каждого свойства объекта содержащее число в переменную sum. Затему функция возвращает сумму всех числовых свойств объекта

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,13 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+'use strict';
 function sumSalary(salaries) {
-
+	let sum = 0;
+	for(let key in salaries){
+		if(typeof salaries[key] === 'number' && typeof salaries[key]!=='undefined'){
+			sum+=salaries[key];
+		} 
+	}
+	return sum;
 }
