c997a6a7a85225f7b894acc1564c178e72c87399
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-28 09:51:48 +0300

    подскажите в чем ошибка

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+'use strict';
 function showSalary(data, age) {
+	const namesBalance = data
+	.filter((user)=>{
+		return user.age <= age;
+	})
+	.map((user)=>{
+		return [user.name,user.balance].join(' ');		
+	});
+return namesBalance.join('\n');
 }
