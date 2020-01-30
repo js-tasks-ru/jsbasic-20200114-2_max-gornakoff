b2c142c1c96171a9d76b8aae8f2dfde2ae7abb82
MaxGornakoff <max.gornakoff@yandex.ru>
2020-01-31 00:29:26 +0300

    Разобрал строку на символы, и с помощью метода replace заменил все запятые на пробелы. Затем создал массив из строки разделив элементы массива с помощью метода split(в качестве разделителя использовал пробел). Затем, с помощью метода фильтр отфильтровал массив, оставив в нем только Boolean и Number значения. Из получившегося массива с помощью методов Math.max и Math.min, так же дополнительного метода apply вычислил Минимальное и максимальное числовые значения вводимой строки. Создал объект и передал в него эти данные.

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,22 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+'use strict';
 function getMinMax(str) {
+	let word = '';
+	for(let ch of str){
+		word = word + ch.replace(',',' ');
+		}
+	let myArray = word.split(' ');
+	let newArr = myArray.filter(Boolean).map(item=>+item);
+	newArr = newArr.filter(Number);
+	let minV = Math.min.apply(null, newArr);
+	let maxV = Math.max.apply(null, newArr);
+	let minMax={
+				min:minV,
+				max:maxV,
+			};
+				
+	return minMax;
 }
+
