a376beacfe1974256baeac6dd222735d2f3a3fc8
MaxGornakoff <max.gornakoff@yandex.ru>
2020-02-05 22:21:24 +0300

    с помощью крутейшего метода .map создал массив arr состоящий из элементов, включающих в себя строчные значения элементов объектов массива friends, разделенных пробелом. Создал html элемент ul и с помощью цикла for создал элементы li, которые, с помощью innerHTML берут элементы массива arr и записывают их в li по порядку и, в конце каждой итерации цикла, li добавляются в ul по очереди.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+let arr = friends.map(item=>item.firstName + ' ' + item.lastName);
+let items = document.createElement('ul');
+
+for(let i = 0; i < arr.length; i++){
+	
+	let item = document.createElement('li');
+	item.innerHTML = arr[i];
+	items.append(item);
+}
+return items;
 }
